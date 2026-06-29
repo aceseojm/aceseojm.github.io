6a57c7677a4433392b1524479cbe9771a44c0ef2
jeongmin <jeongmin@jeongminui-MacBookAir.local>
2026-06-29 13:49:48 +0900

    Replace root site with latest landing page

---
+++ lead-config.js
@@ -0,0 +1,5 @@
+window.BraincityLeadConfig = {
+  // Apps Script 웹앱 배포 URL을 입력하세요.
+  // 예: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxx/exec
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxR-UteZPjeUCFBFLSzaiei9rRiOFIZqsPnFKF8psQUCP2CTt8MNCs0SIpIvTWznsciNA/exec"
+};
